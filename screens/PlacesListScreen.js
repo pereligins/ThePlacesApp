44355e97f43ad27e5b08772ad405fe18a6798698
Aleksey Perelygin <pereligins@gmail.com>
2020-03-02 22:04:12 +0300

    Forms

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import HeaderButton from '../components/HeaderButton';
 
 const PlacesListScreen = props => {
-  return (
-    <View>
-      <Text>PlacesListScreen</Text>
-    </View>
-  );
+    return (
+        <View>
+            <Text>PlacesListScreen</Text>
+        </View>
+    );
 };
 
-PlacesListScreen.navigationOptions = {
-    headerTitle: 'All Places'
+PlacesListScreen.navigationOptions = navData => {
+    return {
+        headerTitle: 'All Places',
+        headerRight: (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                    title="Add Place"
+                    iconName={Platform.OS === 'android' ? 'md-add' : 'ios-add'}
+                    onPress={() => {
+                        navData.navigation.navigate('NewPlace');
+                    }}
+                />
+            </HeaderButtons>
+        )
+    };
 };
 
 const styles = StyleSheet.create({});
