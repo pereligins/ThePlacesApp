bbdd968d2f5a80ad1eefe903a895743dca37b1a8
Aleksey Perelygin <pereligins@gmail.com>
2020-03-03 22:39:05 +0300

    Creating places

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,16 +1,27 @@
 import React, {useState} from 'react';
 import {ScrollView, StyleSheet, Text, TextInput, View, Button} from 'react-native';
 import Colors from "../constants/Colors";
+import {useDispatch} from "react-redux";
+import * as placesActions from '../store/places-actions';
+import ImagePicker from '../components/ImagePicker';
 
 const NewPlaceScreen = props => {
     const [titleValue, setTitleValue] = useState('');
+    const [selectedImage, setSelectedImage] = useState('');
+
+    const dispatch = useDispatch();
 
     const titleChangeHandler = text => {
         setTitleValue(text);
     }
 
-    const savePlaceHandler = () => {
+    const imageTakenHandler = imagePath => {
+        setSelectedImage(imagePath);
+    }
 
+    const savePlaceHandler = () => {
+        dispatch(placesActions.addPlace(titleValue, selectedImage));
+        props.navigation.goBack();
     }
 
     return (
@@ -18,6 +29,7 @@ const NewPlaceScreen = props => {
             <View style={styles.form}>
                 <Text style={styles.label}>Title</Text>
                 <TextInput style={styles.textInput} onChangeText={titleChangeHandler} value={titleValue} />
+                <ImagePicker onImageTaken={imageTakenHandler} />
                 <Button title='Save Place' color={Colors.primary} onPress={savePlaceHandler} />
             </View>
         </ScrollView>
